a786243b9802927f1594351e975c3f198890ee8c
Jonathan Boulay <jonathanboulay@outlook.com>
2022-03-01 15:11:14 +0900

    Add a function that plays a single round

--- game.js
+++ game.js
@@ -3,4 +3,39 @@ const computerPlay = () => {
   return options[Math.floor(Math.random() * options.length)]
 }
 
-console.log(computerPlay())
+const sanitizeSelection = (playerSelection) => {
+  const selectionLowerCase = playerSelection.toLowerCase()
+  return `${selectionLowerCase
+    .charAt(0)
+    .toUpperCase()}${selectionLowerCase.slice(1)}`
+}
+
+const playRound = (playerSelection, computerSelection) => {
+  const sanitizedPlayerSelection = sanitizeSelection(playerSelection)
+
+  if (sanitizedPlayerSelection === computerSelection) return "It's a Draw!"
+
+  if (sanitizedPlayerSelection === 'Rock') {
+    return computerSelection === 'Paper'
+      ? 'You Lose! Paper beats Rock'
+      : 'You Win! Rock beats Scissors'
+  }
+
+  if (sanitizedPlayerSelection === 'Paper') {
+    return computerSelection === 'Scissors'
+      ? 'You Lose! Scissors beats Paper'
+      : 'You Win! Paper beats Rock'
+  }
+
+  if (sanitizedPlayerSelection === 'Scissors') {
+    return computerSelection === 'Rock'
+      ? 'You Lose! Rock beats Scissors'
+      : 'You Win! Scissors beats Paper'
+  }
+}
+
+const playerSelection = 'RoCk'
+const computerSelection = computerPlay()
+console.log('Computer:', computerSelection)
+
+console.log(playRound(playerSelection, computerSelection))
